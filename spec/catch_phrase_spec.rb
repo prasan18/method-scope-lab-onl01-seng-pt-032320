cf0a0e3898a8613c9b452abe5b3bc722d83ee7d3
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-04 16:58:38 +0000

    Automatically backed up by Learn

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -1,6 +1,5 @@
 require "spec_helper"
 describe "#catch_phrase (phrase = "It's-a me, Mario!" do 
-  it "puts out a catch phrase" do 
-    expect{catch_phrase}.to output("It's-a me, Mario!\n").to_stdout
-  end
+puts phrase
 end
+
